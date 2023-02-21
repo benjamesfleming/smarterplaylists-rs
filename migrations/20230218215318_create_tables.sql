e6ded8be11398bffb6fea29ec1544e53f8c2a4fd
Ben Fleming <ben@benfleming.nz>
2023-02-21 21:13:09 +1300

    server/auth: complete session-based login flow

--- migrations/20230218215318_create_tables.sql
+++ migrations/20230218215318_create_tables.sql
@@ -1,8 +1,8 @@
 -- Add migration script here
 CREATE TABLE users (
-    id                    INT(11) PRIMARY KEY,
+    id                    INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     spotify_username      VARCHAR(255) NOT NULL,
     spotify_email         VARCHAR(255) NOT NULL,
-    spotify_access_token  TEXT,
-    spotify_refresh_token TEXT
+    spotify_access_token  TEXT
 );
+CREATE UNIQUE INDEX users_email ON users (spotify_email);
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,9 @@ mod api {
     use crate::db::models::*;
     use crate::{assets, ApplicationState};
 
-    use actix_web::{get, web, HttpResponse, Responder};
+    use actix_session::Session;
+    use actix_web::{get, web, HttpResponse, HttpResponseBuilder, Responder};
+    use rspotify::prelude::*;
     use serde::Deserialize;
 
     #[derive(Deserialize)]
@@ -27,16 +29,39 @@ mod api {
 
     // API Endpoints
 
-    #[get("/api/v1/users/list")]
-    pub async fn v1_users_list_handler(app: web::Data<ApplicationState>) -> impl Responder {
-        let users = sqlx::query_as!(User, "SELECT * FROM users")
-            .fetch_all(&app.db)
-            .await;
-
-        web::Json(users.unwrap())
-    }
-
     // Auth Endpoints
+
+    #[get("/auth/me")]
+    pub async fn auth_me_handler(
+        session: Session,
+        app: web::Data<ApplicationState>,
+    ) -> impl Responder {
+        let user_id = match session.get::<i64>("user_id") {
+            Ok(Some(id)) => id,
+            Ok(None) => {
+                return HttpResponse::Unauthorized().finish();
+            }
+            Err(_) => {
+                return HttpResponse::InternalServerError().finish();
+            }
+        };
+        let user = sqlx::query!("SELECT * FROM users WHERE id = ?", user_id)
+            .map(|row| {
+                let token: rspotify::Token =
+                    serde_json::from_str(&row.spotify_access_token.unwrap()).unwrap();
+                User {
+                    id: row.id,
+                    spotify_username: row.spotify_username,
+                    spotify_email: row.spotify_email,
+                    spotify_access_token: Some(token),
+                }
+            })
+            .fetch_one(&app.db)
+            .await
+            .unwrap();
+
+        HttpResponse::Ok().json(user)
+    }
 
     macro_rules! err_unknown_provider {
         ($provider: expr) => {
@@ -59,6 +84,8 @@ mod api {
 
     #[get("/auth/{provider}/callback")]
     pub async fn auth_provider_sso_callback_handler(
+        session: Session,
+        app: web::Data<ApplicationState>,
         path: web::Path<String>,
         params: web::Query<AuthProviderCallbackParams>,
     ) -> impl Responder {
@@ -66,12 +93,59 @@ mod api {
             // Handle Spotify SSO Callback
             // TODO: Save credentials to the database, start the user session, and rediect back home
             "spotify" => {
-                if let Ok(token) = crate::spotify::auth::request_token(&params.code) {
-                    HttpResponse::Ok()
-                        .body(format!("Access Token: {}", token.unwrap().access_token))
-                } else {
-                    HttpResponse::InternalServerError().finish()
+                // Request the access/secret token
+                if let Ok(Some(token)) = crate::spotify::auth::request_token(&params.code) {
+                    // Request the user data
+                    if let Ok(user) = crate::spotify::init(Some(token.clone())).me() {
+                        let token_json = serde_json::to_string(&token).unwrap();
+                        // Check if we already know that user
+                        // If not, insert the initial database record
+                        let query = sqlx::query!(
+                            "SELECT id FROM users WHERE spotify_email = ?",
+                            user.email
+                        );
+                        let user_id: i64 = match query.fetch_optional(&app.db).await {
+                            // We do know this user, just replace the access token
+                            Ok(Some(user)) => {
+                                let query = sqlx::query!(
+                                    "UPDATE users SET spotify_access_token = ? WHERE id = ?",
+                                    token_json,
+                                    user.id
+                                );
+                                // Update access token into the database
+                                match query.execute(&app.db).await {
+                                    Ok(_) => user.id,
+                                    Err(_) => return HttpResponse::InternalServerError().finish(),
+                                }
+                            }
+                            // We don't know this user
+                            Ok(None) => {
+                                let query =
+                                    sqlx::query!(
+                                        "INSERT INTO users (spotify_username, spotify_email, spotify_access_token) VALUES (?, ?, ?)",
+                                        user.display_name,
+                                        user.email,
+                                        token_json
+                                    );
+                                // Insert user data into the database
+                                match query.execute(&app.db).await {
+                                    Ok(res) => res.last_insert_rowid(),
+                                    Err(_) => return HttpResponse::InternalServerError().finish(),
+                                }
+                            }
+                            // Query Failed
+                            Err(_) => return HttpResponse::InternalServerError().finish(),
+                        };
+                        // Save the user id into the session cookie
+                        if let Ok(_) = session.insert("user_id", user_id) {
+                            // Redirect the user to the home page
+                            return HttpResponse::TemporaryRedirect()
+                                .insert_header(("Location", "/"))
+                                .finish();
+                        }
+                    }
                 }
+                return HttpResponse::InternalServerError().finish();
             }
 
             provider => err_unknown_provider!(provider),
@@ -81,15 +155,16 @@ mod api {
 
 mod db {
     pub mod models {
+        use rspotify::Token;
+        use serde::{Deserialize, Serialize};
 
         /// User holds the details of an authenticated spotify user
-        #[derive(serde::Serialize)]
+        #[derive(Serialize, Deserialize)]
         pub struct User {
-            pub id: Option<i64>,
+            pub id: i64,
             pub spotify_username: String,
             pub spotify_email: String,
-            pub spotify_access_token: Option<String>,
-            pub spotify_refresh_token: Option<String>,
+            pub spotify_access_token: Option<Token>,
         }
     }
 }
@@ -127,6 +202,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(state.clone())
             .service(api::auth_provider_sso_redirect_handler)
             .service(api::auth_provider_sso_callback_handler)
+            .service(api::auth_me_handler)
             .service(api::index_get_handler)
     })
     .bind("127.0.0.1:8080")?
