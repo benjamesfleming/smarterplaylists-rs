30e317201943d7bda4874729bf82e32d6f940ca1
benjamesfleming <ben@benfleming.nz>
2023-05-20 14:46:24 +1200

    server/api: cache user_playlists for 5 minutes

--- src/handlers/api_spotify.rs
+++ src/handlers/api_spotify.rs
@@ -2,7 +2,7 @@ use actix_session::Session;
 use actix_web::{get, web, Responder};
 use rspotify::{model::SimplifiedPlaylist, prelude::*};
 
-use crate::{error::PublicError, macros, models::User, spotify, ApplicationState};
+use crate::{error::PublicError, macros, models::User, spotify, ApplicationState, cache};
 
 #[get("/api/v1/spotify/user_playlists")]
 pub async fn api_v1_spotify_user_playlists(
@@ -11,14 +11,18 @@ pub async fn api_v1_spotify_user_playlists(
 ) -> Result<impl Responder, PublicError> {
     let user_id = macros::user_id!(session);
     let user = sqlx::query_as::<_, User>("SELECT * FROM users WHERE spotify_id = ?")
-        .bind(user_id)
+        .bind(&user_id)
         .fetch_one(&app.db)
         .await?;
 
-    let mut playlists: Vec<SimplifiedPlaylist> = Vec::new();
-    for plst in spotify::init(user.token()).user_playlists(user.id()) {
-        playlists.push(plst.unwrap());
-    }
+    let key = format!("user_playlists:{user_id}");
+    let res = cache::get_or_create(&app.cache, key.as_str(), 300, false, || {
+        let mut playlists: Vec<SimplifiedPlaylist> = Vec::new();
+        for plst in spotify::init(user.token()).user_playlists(user.id()) {
+            playlists.push(plst?);
+        }
+        Ok(playlists)
+    }).await?;
 
-    Ok(web::Json(playlists))
+    Ok(web::Json(res))
 }
