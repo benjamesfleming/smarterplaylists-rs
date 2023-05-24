f525df376c2a623dde4215c4466d493f11e55b6e
benjamesfleming <ben@benfleming.nz>
2023-05-24 20:42:20 +1200

    server/components: use generic component trait

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -6,3 +6,36 @@ pub mod combiners;
 pub mod conditinals;
 pub mod filters;
 pub mod sources;
+
+use rspotify::AuthCodeSpotify as Client;
+use serde::Deserialize;
+
+use crate::error::PublicError;
+
+use self::sources::*;
+
+pub trait Component<T> {
+    fn execute(client: &Client, args: T, prev: Vec<TrackList>) -> Result<TrackList, rspotify::ClientError>;
+}
+
+// --
+
+macro_rules! components {
+    ( $(($a:literal, $b:ty, $c:ty)),* ) => {
+        // Execute a component by name -
+        // n.b This function takes a generic args value and will fail if it can't be deserilized into the correct type
+        pub fn execute(id: &str, client: &Client, args: serde_json::Value, prev: Vec<TrackList>) -> Result<TrackList, PublicError> {
+            match id {
+                $(
+                    $a => <$b>::execute(client, <$c as Deserialize>::deserialize(args)?, prev).map_err(|_| "".into()),
+                )*
+                _ => Err(format!("invalid component type '{}' provided", id).into()),
+            }
+        }
+    };
+}
+
+components! [
+    ("source:artist_top_tracks", ArtistTopTracks, ArtistTopTracksArgs),
+    ("source:album", Album, AlbumArgs)
+];
--- src/components/sources.rs
+++ src/components/sources.rs
@@ -1,39 +1,48 @@
 ///! Sources take user-defined arguments and return TrackLists
-use rspotify::model::Market;
+use rspotify::model::*;
 use rspotify::prelude::*;
 use rspotify::AuthCodeSpotify as Client;
 
-use super::TrackList;
+use serde::Deserialize;
 
-pub trait Source {
-    fn source(&self, client: &Client) -> Result<TrackList, rspotify::ClientError>;
+use super::*;
+
+#[derive(Deserialize)]
+pub struct AlbumArgs {
+    pub id: String,
 }
 
-//
+pub struct Album;
 
-pub type Album = rspotify::model::FullAlbum;
-
-impl Source for Album {
+impl Component<AlbumArgs> for Album {
     // Fetch the list of tracks in the album, then
     // request the FullTrack object
-    fn source(&self, client: &Client) -> Result<TrackList, rspotify::ClientError> {
+    fn execute(client: &Client, args: AlbumArgs, _: Vec<TrackList>) -> Result<TrackList, rspotify::ClientError> {
         let mut ids = Vec::new(); // Temp track id vector
-        for t in client.album_track(self.id.to_owned()) {
+        for t in client.album_track(AlbumId::from_id_or_uri(&args.id).unwrap()) {
             ids.push(t.unwrap().id.unwrap())
         }
         client.tracks(ids, None)
     }
 }
 
-//
+// --
 
-pub type Artist = rspotify::model::FullArtist;
+#[derive(Deserialize)]
+pub struct ArtistTopTracksArgs {
+    pub id: String,
+}
 
-impl Source for Artist {
+pub struct ArtistTopTracks;
+
+impl Component<ArtistTopTracksArgs> for ArtistTopTracks {
     // Fetch top tracks for a given artist
     // Note: This selects the artists top tracks, not all of them
-    fn source(&self, client: &Client) -> Result<TrackList, rspotify::ClientError> {
-        client.artist_top_tracks(self.id.to_owned(), Market::FromToken)
+    fn execute(client: &Client, args: ArtistTopTracksArgs, _: Vec<TrackList>) -> Result<TrackList, rspotify::ClientError> {
+        client.artist_top_tracks(
+            ArtistId::from_id_or_uri(&args.id).unwrap(),
+            Market::FromToken,
+        )
     }
 }
 
